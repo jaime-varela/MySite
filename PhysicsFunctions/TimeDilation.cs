dd8c5aeb01dc4fcc226c1ed7ae0f6a3991860114
jaime-varela <varela.jaime01@gmail.com>
2018-05-29 12:33:18 -0400

    added a class

---
+++ PhysicsFunctions/TimeDilation.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PhysicsFunctions
+{
+    public static class TimeDilation
+    {
+        public static decimal TwinParadox(decimal velocity)
+        {
+            return 0;
+        }
+    }
+}
