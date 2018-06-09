9feb8422b034a6fd577f2c00e74ea4035ccba589
jaime-varela <varela.jaime01@gmail.com>
2018-06-08 19:39:28 -0500

    added basic math controller

---
+++ JaimeVarela/Controllers/BasicMathController.cs
@@ -0,0 +1,13 @@
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using JaimeVarela.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JaimeVarela.Controllers
+{
+    public class BasicMathController : Controller
+    {
+        
+    }
+}
